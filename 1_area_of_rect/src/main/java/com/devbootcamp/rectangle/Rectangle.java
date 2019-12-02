ba7da9c1990d3e899cbb762b929952ff436f21cc
Nipun Kanade <kanadenipun@gmail.com>
2019-12-02 21:01:20 +0530

    [Nipun] Rectangle class returns area for a given rectangle

--- 1_area_of_rect/src/main/java/com/devbootcamp/rectangle/Rectangle.java
+++ 1_area_of_rect/src/main/java/com/devbootcamp/rectangle/Rectangle.java
@@ -3,11 +3,17 @@ package com.devbootcamp.rectangle;
 // understands a polygon with perpendicular adjacent sides
 public class Rectangle {
 
+    private int length;
+    private int breadth;
+
     public Rectangle(int length, int breadth) {
 
+
+        this.length = length;
+        this.breadth = breadth;
     }
 
     public int area() {
-        return 6;
+        return length * breadth;
     }
 }
--- 1_area_of_rect/src/test/java/com/devbootcamp/rectangle/RectangleTest.java
+++ 1_area_of_rect/src/test/java/com/devbootcamp/rectangle/RectangleTest.java
@@ -7,4 +7,9 @@ class RectangleTest {
     void expectAreaToBe6WhenLength3andBreadth2() {
         assertEquals(6, new Rectangle(3,2).area());
     }
+
+    @Test
+    void expectAreaToBe28WhenLength7andBreadth4() {
+        assertEquals(28, new Rectangle(7,4).area());
+    }
 }
